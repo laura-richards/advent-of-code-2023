72185aa24bba2b7ab0f5db6dac907c28babb48e7
Laura Richards <lionheartnz85@gmail.com>
2023-12-18 19:13:37 +1300

    solve day 2

--- code/Day2/day2.mjs
+++ code/Day2/day2.mjs
@@ -1,4 +1,4 @@
-export { calculateGame, calculateTotal }
+export { calculateGame, calculateTotal, calculatePower, calculatePowers }
 import { readFileAsync } from '../../fileUtils.mjs'
 
 function calculateGame(game) {
@@ -44,6 +44,41 @@ function calculateGame(game) {
   return id
 }
 
+function calculatePowers(game) {
+  const splitGame = game.split(':')
+  let id = Number(splitGame[0].replace(/Game/, ''))
+  const rounds = splitGame[1].split(';')
+  const combinedList = rounds.join(',').split(',')
+  let red = []
+  let blue = []
+  let green = []
+  combinedList.map((item) => {
+    if (item.includes('blue')) {
+      const num = Number(item.replace(/\D/g, ''))
+      blue.push(num)
+    }
+    if (item.includes('red')) {
+      const num = Number(item.replace(/\D/g, ''))
+      red.push(num)
+    }
+    if (item.includes('green')) {
+      const num = Number(item.replace(/\D/g, ''))
+      green.push(num)
+    }
+  })
+  const greenValue = green.reduce(
+    (acc, cur) => (cur < acc ? (cur = acc) : cur),
+    0
+  )
+  const redValue = red.reduce((acc, cur) => (cur < acc ? (cur = acc) : cur), 0)
+  const blueValue = blue.reduce(
+    (acc, cur) => (cur < acc ? (cur = acc) : cur),
+    0
+  )
+  console.log(greenValue, redValue, blueValue)
+  return greenValue * blueValue * redValue
+}
+
 function calculateTotal(game) {
   const splitGames = game.split('\n')
   const gameValues = []
@@ -52,5 +87,12 @@ function calculateTotal(game) {
   return total
 }
 
+function calculatePower(game) {
+  const splitGames = game.split('\n')
+  const gameValues = []
+  splitGames.map((game) => gameValues.push(calculatePowers(game)))
+  const total = gameValues.reduce((acc, curr) => acc + curr, 0)
+  return total
+}
 const data = await readFileAsync('./code/Day2/day2-data.txt')
-console.log(calculateTotal(data))
+console.log(calculatePower(data))
--- code/Day2/test/day2.test.mjs
+++ code/Day2/test/day2.test.mjs
@@ -1,5 +1,10 @@
 import { describe, test, expect } from 'vitest'
-import { calculateGame, calculateTotal } from '../day2.mjs'
+import {
+  calculateGame,
+  calculateTotal,
+  calculatePowers,
+  calculatePower,
+} from '../day2.mjs'
 
 const game1 = 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green'
 const game2 = 'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue'
@@ -41,26 +46,26 @@ describe('CalculateSum works out the total for part 1', () => {
 
 describe('Correctly calculates day 2 part 2 game lines', () => {
   test('calculateValue works out value of Game1', () => {
-    const game1Value = calculatePower(game1)
+    const game1Value = calculatePowers(game1)
     expect(game1Value).toBe(48)
   })
   test('calculateValue works out value of Game2', () => {
-    const game1Value = calculatePower(game2)
+    const game1Value = calculatePowers(game2)
     expect(game1Value).toBe(12)
   })
   test('calculateValue works out value of Game3', () => {
-    const game1Value = calculatePower(game3)
+    const game1Value = calculatePowers(game3)
     expect(game1Value).toBe(1560)
   })
   test('calculateValue works out value of Game4', () => {
-    const game1Value = calculatePower(game4)
+    const game1Value = calculatePowers(game4)
     expect(game1Value).toBe(630)
   })
 })
 
 describe('CalculateSum works out the total for part 2', () => {
   test('Use calculateSum to work out sum', () => {
-    const sampleValue = calculateTotal(fullSample)
+    const sampleValue = calculatePower(fullSample)
     expect(sampleValue).toBe(2286)
   })
 })
