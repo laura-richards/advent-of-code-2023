04c6a8a758549361f29fd0e567f0c4b1e8738281
Laura Richards <lionheartnz85@gmail.com>
2024-01-01 01:14:26 +1300

    day 4 part 2

--- code/Day4/day4.mjs
+++ code/Day4/day4.mjs
@@ -1,7 +1,6 @@
 export { calculateGame, calculateTotal }
 import { readFileAsync } from '../../fileUtils.mjs'
 
-
 function calculateGame(game) {
   let total = 0
   const splitCard = game.split(': ')
@@ -10,29 +9,46 @@ function calculateGame(game) {
   const gameNums = splitGame[1].split(' ')
   // console.log(gameNums)
   // console.log(winningNums)
-  winningNums.map((num) => {
-    if (num === '') { return}
-   if (gameNums.includes(num)) {
-    if (total === 0 ) {
-      total = 1 
+
+  const matches = winningNums.map((num) => {
+    if (num === '') {
       return
     }
-    total = total * 2
-    return
-  }})  
-  // console.log(total)
-  return total
+    if (gameNums.includes(num)) {
+      if (total === 0) {
+        total = 1
+        return
+      }
+      total++
+    }
+    return total
+  })
+  // console.log(matches)
+  return { id: splitCard[0], matches: total, copies: 1 }
+  // return total
 }
 
 function calculateTotal(data) {
-    const splitGames = data.split('\n')
-    let total = 0
-    splitGames.map((game) => (
-      // console.log(total),
-      total += calculateGame(game)))
-    return total
-  }
+  const splitGames = data.split('\n')
+  let total = 0
+  const cards = splitGames.map((game) =>
+    // console.log(total),
+    calculateGame(game)
+  )
+  // console.log(cards)
+  cards.forEach((card, index) => {
+    for (let i = 0; i < card.copies; i++) {
+      for (let j = 0; j < card.matches; j++) {
+        let nextCard = cards[index + j + 1]
+        nextCard.copies = nextCard.copies + 1
+        // console.log(nextCard.id, nextCard.copies, j)
+      }
+    }
+  })
+  // console.log(cards)
+  total = cards.reduce((acc, cur) => acc + cur.copies, 0)
+  return total
+}
 
-
-  const data = await readFileAsync('./code/Day4/day4data.txt')
+const data = await readFileAsync('./code/Day4/day4data.txt')
 console.log(calculateTotal(data))
